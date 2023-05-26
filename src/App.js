a279998a84e04db1a0fbfd407c22d5c1016d1d30
Jasurbek Aripov <jasurbek_aripov@mail.ru>
2023-05-26 19:31:53 +0500

    adding login/sign in pages & server side with db

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import RegisterPage from "./component/RegisterPage";
+import LoginPage from "./component/UI/LoginPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <RegisterPage />
+      {/* <AuthPage /> */}
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
